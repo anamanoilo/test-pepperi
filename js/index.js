f89cbf09ae55de70cdabe3b9b185b42e5ad47770
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2022-03-30 01:32:02 +0200

    final commit

--- js/index.js
+++ js/index.js
@@ -1,118 +1,139 @@
 const refs = {
   form: document.querySelector("form"),
   list: document.querySelector(".list"),
-  sortByNameBtn: document.querySelector(".sort-by-name-btn"),
-  sortByValueBtn: document.querySelector(".sort-by-value-btn"),
-  deleteBtn: document.querySelector(".delete-btn"),
-  xmlBtn: document.querySelector(".xml-btn"),
-  buttons: document.querySelectorAll(".btn"),
+  buttons: document.querySelector(".buttons-wrapper"),
+  error: document.querySelector(".error"),
 };
 
-refs.form.addEventListener("submit", onSubmit);
 const data = [];
+refs.form.addEventListener("submit", onSubmit);
+refs.buttons.addEventListener("click", onClick);
 
 function onSubmit(e) {
   e.preventDefault();
 
-  const input = refs.form.elements.input;
+  const input = e.currentTarget.elements.input;
   const inputValue = input.value.trim();
   if (!inputValue) {
-    alert("Enter some text!");
+    e.currentTarget.classList.add("form--error");
+    refs.error.textContent = "Enter some text!";
     return;
   }
 
   const pair = inputValue.split("=");
   if (pair.length !== 2) {
-    alert('Wrong format. Please fill the field in the format "Name=Value".');
+    e.currentTarget.classList.add("form--error");
+    refs.error.textContent =
+      'Wrong format. Please fill the field in the format "Name=Value".';
     return;
   }
   const pairObj = {
     name: pair[0].trim(),
     value: pair[1].trim(),
   };
-
   for (const key in pairObj) {
     if (pairObj.hasOwnProperty(key)) {
       if (!pairObj[key]) {
         const capitalizedKey = key[0].toUpperCase() + key.slice(1);
-        alert(
-          `Wrong format. Please make sure to include ${capitalizedKey} in the Name=Value Pair.`
-        );
+        refs.form.classList.add("form--error");
+        refs.error.textContent = `Wrong format. Please make sure to include ${capitalizedKey} in the Name=Value Pair.`;
         return;
       }
       const alphanumeric = /^[\p{L}\p{N}]*$/u;
       if (!pairObj[key].match(alphanumeric)) {
-        alert(
-          "Wrong format. Names and Values can contain only alpha-numeric characters."
-        );
+        refs.form.classList.add("form--error");
+        refs.error.textContent =
+          "Wrong format. Names and Values can contain only alpha-numeric characters.";
         return;
       }
     }
   }
-
   data.push(pairObj);
+  e.currentTarget.classList.remove("form--error");
+  refs.error.textContent = "";
   renderList(data);
-  refs.buttons.forEach((btn) => (btn.disabled = false));
+  refs.buttons.childNodes.forEach((btn) => (btn.disabled = false));
+
   refs.form.reset();
 }
 
-// function addToList(pairObj) {
-//   const item = document.createElement("li");
-//   item.textContent = `${pairObj.name}=${pairObj.value}`;
-//   refs.list.append(item);
-// }
+function onClick(e) {
+  if (e.target === e.currentTarget) {
+    return;
+  }
 
-refs.deleteBtn.addEventListener("click", onDelete);
+  if (e.target.dataset.action === "name") {
+    sortByName();
+    return;
+  }
+  if (e.target.dataset.action === "value") {
+    sortByValue();
+    return;
+  }
+  if (e.target.dataset.action === "delete") {
+    clearList();
+    return;
+  }
+  if (e.target.dataset.action === "xml") {
+    showXml();
+    return;
+  }
+}
 
-function onDelete() {
+function sortByName() {
+  // the original array is mutated to have possibility show the sorted array in XML
+  // ----------------- Sort by the name length-----------------------
+  // data.sort((a, b) => a.name.length - b.name.length);
+  // renderList(data);
+  // ----------------- Sort by alphabet-----------------------
+  data.sort(function (a, b) {
+    return a.name.localeCompare(b.name);
+  });
+  renderList(data);
+}
+
+function sortByValue() {
+  // ----------------- Sort by the name length-----------------------
+  // data.sort((a, b) => a.value.length - b.value.length);
+  // renderList(data);
+  // ----------------- Sort by alphabet-----------------------
+  data.sort(function (a, b) {
+    return a.value.localeCompare(b.value);
+  });
+  renderList(data);
+}
+
+function clearList() {
   refs.list.innerHTML = "";
   data.length = 0;
-  refs.buttons.forEach((btn) => (btn.disabled = true));
+  refs.buttons.childNodes.forEach((btn) => (btn.disabled = true));
 }
 
-refs.sortByNameBtn.addEventListener("click", onSortByName);
-refs.sortByValueBtn.addEventListener("click", onSortByValue);
-
-function onSortByName() {
-  // const sortedData = [...data].sort((a, b) => a.name.length - b.name.length);
-  // renderList(sortedData);
-  data.sort((a, b) => a.name.length - b.name.length);
-  renderList(data);
-}
-
-function onSortByValue() {
-  // const sortedData = [...data].sort((a, b) => a.value.length - b.value.length);
-  // renderList(sortedData);
-  data.sort((a, b) => a.value.length - b.value.length);
-  renderList(data);
+function showXml() {
+  const markup = data
+    .map(
+      ({ name, value }) => `
+<li>
+<code>
+<pre>&ltitem&gt
+  &ltname&gt${name}&lt/name&gt
+  &ltvalue&gt${value}&lt/value&gt
+&lt/item&gt</pre>
+</code>
+</li>`
+    )
+    .join("");
+  refs.list.innerHTML = "";
+  refs.list.insertAdjacentHTML("afterbegin", markup);
 }
 
 function renderList(dataOfObjects) {
   const markup = dataOfObjects
     .map(({ name, value }) => {
       return `<li>
-              ${name}=${value}
+                <p>${name}=${value}</p>
             </li>`;
     })
     .join("");
   refs.list.innerHTML = markup;
-}
-
-refs.xmlBtn.addEventListener("click", onXmlBtn);
-
-function onXmlBtn() {
-  const markup = data
-    .map(({ name, value }) => {
-      return `<li>
-              <code>
-              	&ltitem&gt <br>
-              	&nbsp;&nbsp; &ltname&gt${name}&lt/name&gt <br>
-              	&nbsp;&nbsp; &ltvalue&gt${value}&lt/value&gt <br>
-              	&lt/item&gt
-              </code>
-            </li>`;
-    })
-    .join("");
-  refs.list.innerHTML = "";
-  refs.list.insertAdjacentHTML("afterbegin", markup);
 }
